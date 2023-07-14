eefbd69e3bbe749bd452f247d88b979986b033e7
Pedro Iriano <pdzeus83aw@gmail.com>
2023-07-14 21:43:07 +0700

    Add Detail Pages

--- resources/views/pages/publications/news-detail.blade.php
+++ resources/views/pages/publications/news-detail.blade.php
@@ -1,53 +1,77 @@
 @extends('layouts.page')
 
 @section('content')
-@include('includes.float')
-<section class="bg-white py-10">
-    @inject('Carbon', 'Carbon\Carbon')
-    <div class="container px-5 mt-5">
-        <div class="row gx-5 justify-content-center">
+@inject('Carbon', 'Carbon\Carbon')
+<!-- Start -->
+<section class="relative table w-full md:pb-24 pb-16 mt-28">
+    <div class="container">
+        <div class="grid md:grid-cols-12 grid-cols-1 gap-[30px]">
             @if(!empty($newsDetails))
                 @foreach($newsDetails as $newsDetail)
-                    <div class="col-lg-10 col-xl-8">
-                        @php
-                            $publishDate = $Carbon::parse($newsDetail['tgl_publish']);
-                            $publishDate = $publishDate->format('d-m-Y');
-                        @endphp
-                        <div class="single-post">
+                    @php
+                        $publishDate = $Carbon::parse($newsDetail['tgl_publish']);
+                        $publishDate = $publishDate->format('d-m-Y');
+                    @endphp
+                    <div class="lg:col-span-6 md:col-span-5">
+                        <div class="sticky top-20">
                             @if (!empty($newsDetail['lampiran']))
-                                <img class="img-fluid mb-2 rounded mx-auto d-block w-100" src="https://cms.depok.go.id/upload/{{ $newsDetail['lampiran'] }}" />
-                                <div class="small text-gray-500 text-center mb-5">Foto: DISKOMINFO Kota Depok</div>
+                                <img loading="lazy" src="https://cms.depok.go.id/upload/{{ $newsDetail['lampiran'] }}" class="rounded-md shadow-md dark:shadow-gray-800" alt="News">
                             @else
-
+                                <img loading="lazy" src="{{ asset('assets/images/page/news.jpg') }}" class="rounded-md shadow-md dark:shadow-gray-800" alt="News">
                             @endif
-                            <h1>{{ $newsDetail['title'] }}</h1>
-                            <p class="lead">
-                                {!! $newsDetail['content'] !!}
-                            </p>
-                            <div class="d-flex align-items-center justify-content-between mb-5">
-                                <div class="single-post-meta me-4">
-                                    <div class="single-post-meta-details">
-                                        <div class="single-post-meta-details-name">DISKOMINFO Kota Depok</div>
-                                        <div class="single-post-meta-details-date">{{ $publishDate }}</div>
+                        </div>
+                    </div>
+                    <div class="lg:col-span-6 md:col-span-7">
+                        <div class="">
+                            <h5 class="lg:text-4xl lg:leading-relaxed text-2xl font-semibold">
+                                {{ $newsDetail['title'] }}
+                            </h5>
+                            <div class="grid grid-cols-1 mt-8">
+                                <ul class="md:w-fit w-full flex-wrap justify-center text-center p-3 bg-white dark:bg-slate-900 shadow dark:shadow-gray-800 rounded-md" id="myTab" data-tabs-toggle="#StarterContent" role="tablist">
+                                    <li role="presentation" class="md:inline-block block md:w-fit w-full">
+                                        <button class="px-6 py-2 font-semibold rounded-md w-full hover:text-indigo-600 transition-all duration-500 ease-in-out" id="detail-tab" data-tabs-target="#detail" type="button" role="tab" aria-controls="detail" aria-selected="true">
+                                            Detail
+                                        </button>
+                                    </li>
+                                    <li role="presentation" class="md:inline-block block md:w-fit w-full">
+                                        <button class="px-6 py-2 font-semibold rounded-md w-full transition-all duration-500 ease-in-out" id="information-tab" data-tabs-target="#information" type="button" role="tab" aria-controls="information" aria-selected="false">
+                                            Informasi
+                                        </button>
+                                    </li>
+                                </ul>
+                                <div id="StarterContent" class="mt-6">
+                                    <div class="" id="detail" role="tabpanel" aria-labelledby="detail-tab">
+                                        <div class="grid grid-cols-1">
+                                            {!! $newsDetail['content'] !!}
+                                        </div>
+                                    </div>
+                                    <div class="hidden" id="information" role="tabpanel" aria-labelledby="information-tab">
+                                        <div class="grid grid-cols-1">
+                                            <div class="flex items-center">
+                                                <div class="relative">
+                                                    <img loading="lazy" src="{{ asset('assets/images/client/01.jpg') }}" class="h-16 w-16 rounded-full shadow-md dark:shadow-gray-800" alt="Author">
+                                                </div>
+                                                <div class="ml-3">
+                                                    <h6 class="text-lg font-semibold">
+                                                        <a class="hover:text-indigo-600 duration-500 ease-in-out">
+                                                            Admin
+                                                        </a>
+                                                    </h6>
+                                                    <span class="text-slate-400">
+                                                        {{ $publishDate }}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="single-post-meta-links">
-                                    <a href="#!"><i class="fab fa-twitter fa-fw"></i></a>
-                                    <a href="#!"><i class="fab fa-facebook-f fa-fw"></i></a>
-                                    <a href="#!"><i class="fas fa-bookmark fa-fw"></i></a>
-                                </div>
                             </div>
-                            <hr class="my-5" />
-                            <div class="text-center"><a class="btn btn-transparent-dark" onclick="history.back()">Kembali ke Berita</a></div>
                         </div>
                     </div>
                 @endforeach
             @endif
         </div>
     </div>
-    <div class="svg-border-rounded text-light">
-        <!-- Rounded SVG Border-->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
-    </div>
 </section>
+<!-- End -->
 @endsection
