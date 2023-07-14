df39713a253cbf8417682e2e12b8081c1efc4a0a
Pedro Iriano <pdzeus83aw@gmail.com>
2023-07-14 19:38:16 +0700

    Add Footer Social Media Links

--- resources/views/includes/page-footer.blade.php
+++ resources/views/includes/page-footer.blade.php
@@ -12,28 +12,32 @@
                             <p class="mt-6 text-gray-300">
                                 Website Resmi {{ $shortWorkUnits }}. Silakan kunjungi Media Sosial Kami untuk informasi lainnya.
                             </p>
-                            <ul class="list-none mt-6">
-                                <li class="inline">
-                                    <a href="" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
-                                        <i class="uil uil-facebook-f align-middle" title="facebook"></i>
-                                    </a>
-                                </li>
-                                <li class="inline">
-                                    <a href="" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
-                                        <i class="uil uil-instagram align-middle" title="instagram"></i>
-                                    </a>
-                                </li>
-                                <li class="inline">
-                                    <a href="" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
-                                        <i class="uil uil-twitter align-middle" title="twitter"></i>
-                                    </a>
-                                </li>
-                                <li class="inline">
-                                    <a href="" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
-                                        <i class="uil uil-youtube align-middle" title="youtube"></i>
-                                    </a>
-                                </li>
-                            </ul>
+                            @if (!empty($generalInformations))
+                                @foreach ($generalInformations as $generalInformation)
+                                    <ul class="list-none mt-6">
+                                        <li class="inline">
+                                            <a href="{{ $generalInformation['Facebook'] }}" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+                                                <i class="uil uil-facebook-f align-middle" title="facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="inline">
+                                            <a href="{{ $generalInformation['Instagram'] }}" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+                                                <i class="uil uil-instagram align-middle" title="instagram"></i>
+                                            </a>
+                                        </li>
+                                        <li class="inline">
+                                            <a href="{{ $generalInformation['Twitter'] }}" target="_blank" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+                                                <i class="uil uil-twitter align-middle" title="twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="inline">
+                                            <a href="{{ $generalInformation['Youtube'] }}" class="btn btn-icon btn-sm border border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+                                                <i class="uil uil-youtube align-middle" title="youtube"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                @endforeach
+                            @endif
                         </div>
 
                         <div class="lg:col-span-2 md:col-span-6">
